7cdd94131f5bba12f91f55c175fa25084a08e5b2
Vallghall <romangusev2001@mail.ru>
2021-08-15 19:53:53 +0300

    Initial commit

---
+++ Weight for weight [5kyu]/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+	"strings"
+)
+
+type elem []string
+
+func (e elem) Len() int { return len(e)}
+func (e elem) Swap(i, j int) { e[i], e[j] = e[j], e[i] }
+func (e elem) Less(i, j int) bool {
+	a, b := e.digitSum(i), e.digitSum(j)
+	if a == b {
+		return e[i] < e[j]
+	} else {
+		return a < b
+	}
+}
+
+func (e elem) digitSum(ind int) int {
+	var temp int
+	for _, val := range e[ind] {
+		temp += int(val - '0')
+	}
+	return temp
+}
+
+func OrderWeight(str string) string {
+	elems := strings.Fields(str)
+	sort.Sort(elem(elems))
+	return strings.Join(elems, " ")
+}
+
+func main() {
+	fmt.Println(OrderWeight("103 123 4444 99 2000"))
+}
+
